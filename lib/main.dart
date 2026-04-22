5bcdcc044b240f74d6e4a1622e01e0c68bc1dc93
Shaurya <tiwarishaurya395@gmail.com>
2026-04-23 00:33:51 +0530

    feat: configure Firebase using FlutterFire CLI and implement platform-specific initialization

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'app.dart';
+import 'firebase_options.dart';
 import 'providers/admin_provider.dart';
 import 'providers/auth_provider.dart';
 import 'providers/donation_provider.dart';
@@ -12,7 +13,9 @@ import 'services/firestore_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   final firestoreService = FirestoreService();
 

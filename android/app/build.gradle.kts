16a11406846a35de6903fa011ecee324e87e6663
Shaurya <tiwarishaurya395@gmail.com>
2026-04-23 00:57:53 +0530

    refactor: update Android application ID and package namespace to com.foodaid.app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.foodaid.food_aid"
+    namespace = "com.foodaid.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.foodaid.food_aid"
+        applicationId = "com.foodaid.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/foodaid/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/foodaid/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.foodaid.food_aid
+package com.foodaid.app
 
 import io.flutter.embedding.android.FlutterActivity
 

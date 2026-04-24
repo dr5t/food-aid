1c74998addc1bc808de9cfce0957985c50b51723
Shaurya <tiwarishaurya395@gmail.com>
2026-04-25 03:15:56 +0530

    feat: add foundation import to firestore_service.dart

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'package:flutter/foundation.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:rxdart/rxdart.dart';
 import '../models/donation_model.dart';
